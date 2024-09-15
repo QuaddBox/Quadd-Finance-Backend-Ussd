a0063412f343332e228a3f4f8d280470f08ea689
Anthonyushie <anthonytwan75official@gmail.com>
2024-09-15 17:07:02 +0100

    updated ussd flow

--- middlewares/ussd/useUserDetails.js
+++ middlewares/ussd/useUserDetails.js
@@ -2,11 +2,13 @@ import User from "../../models/user.js";
 
 export const useUserDetails = async(req,res,next) => {
     const {phoneNumber} = req.body;
+    console.log(phoneNumber);   
     try{
         const queryRes = await User.findOne({
             phoneNumber
         })
-        if(queryRes.data){
+        console.log(queryRes);
+        if(queryRes && queryRes.data){
             req.user = queryRes.data
         }
     }catch(error){
