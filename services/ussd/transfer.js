c3ded56ac20aa39bfa5b2f378d804ad7e53a6c67
Anthonyushie <anthonytwan75official@gmail.com>
2024-09-16 01:57:45 +0100

    updated transfer

--- services/ussd/transfer.js
+++ services/ussd/transfer.js
@@ -7,21 +7,21 @@ export const transferHandler = async ({res,user,data,dataCount})=>{
 
     const responseHandler = new USSDService(res)
 
-    if(dataCount === 1)  return responseHandler.startTransfer()
-    if(dataCount === 2)  return responseHandler.addRecieverCurrency
-    if(dataCount === 3)  return responseHandler.addReciever()
-    if(dataCount === 4)  {
+    if(dataCount === 2)  return responseHandler.startTransfer()
+        if(dataCount === 3)  return responseHandler.addRecieverCurrency()
+        if(dataCount === 4)  return responseHandler.addReciever()
+        if(dataCount === 5)  {
         const phoneNumber = data[dataCount-1]
         const reciever = await User.findOne({phoneNumber})
         if(!reciever) return responseHandler.sendInvalidUserResponse()
         return responseHandler.addAmount
     }
-    if(dataCount === 5){
+    if(dataCount === 6){
         const amount = parseFloat(data[dataCount-1])
         if (user.balance < amount) return responseHandler.sendInsufficientFundsResponse()
         return responseHandler.enterPin()
     }
-    if(dataCount === 6)  {
+    if(dataCount === 7)  {
         if(data[dataCount-1] !== user.pin) return responseHandler.sendIncorrectPinResponse()
         return responseHandler.endTransfer("USDT",30000,+2349034567889)
     }
